624d3e794bcb6aff47fa8265667327535a8a52af
Daniil Tsybarov <daniatsybarov19@gmail.com>
2024-10-11 14:34:28 +0300

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function askQuestion(answer, question) {
+    const userAnswer = prompt(question);
+
+    if (userAnswer.toLowerCase() === answer) {
+        alert("Correct!");
+    } else {
+        alert("Sorry, the correct answer is " + answer);
+    }
+}
+
+function puzzle(){
+    askQuestion('лук', 'Сидит дед во 100 шуб одет, кто его раздевает тот слезы проливает');
+    askQuestion('елка', 'Зимой и летом одним цветом');
+}
